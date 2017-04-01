e6a255b1b857c5789861c8c3125297c87be14742
Roger <weihsuan.li24@gmail.com>
2017-04-01 13:51:36 -0700

    Determine hamster's adoption eligibilty

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,12 @@
 puts "What is the hamster's name?"
 name = gets.chomp
 print name
-end
 
 puts "How loud is the hamster, from a scale of 1 to 10?"
 volume = gets.chomp
 print volume
-end
+
+puts "What color fur does the hamster have?"
+fur_color = gets.chomp
+print fur_color
+
