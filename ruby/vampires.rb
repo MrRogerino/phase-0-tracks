6fccf9dcb031759708e50fd7bc865120fb3bd171
Roger <weihsuan.li24@gmail.com>
2017-04-01 16:46:03 -0700

    Add boolean checks for age, garlic allergy, and immortality

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,9 +6,36 @@ age = gets.chomp
 
 puts "What year were you born?"
 birthyear = gets.chomp
+real_age = 2017 - birthyear.to_i
+puts real_age
 
 puts "Would you like some garlic bread? (Y or N)"
 garlic = gets.chomp
 
 puts "Do you need health insurance? (Y or N)"
 insurance = gets.chomp
+
+#boolean checks
+if age.to_i == real_age 
+	lying = false
+else
+	lying = true
+end
+
+puts lying
+
+if garlic == "Y" or garlic == "y"
+	allergic = true
+else
+	allergic = false
+end
+
+puts allergic
+
+if insurance == "Y" or insurance == "y"
+	immortal = true
+else
+	immortal = false
+end
+
+puts immortal
