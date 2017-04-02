a7dd3b342358243522c79eb46235296bd50e3a7e
Roger <weihsuan.li24@gmail.com>
2017-04-01 18:04:23 -0700

    Convert answers into results

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,11 @@
+vamp_score = 0
+
 puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
 age = gets.chomp
+
 
 puts "What year were you born?"
 birthyear = gets.chomp
@@ -17,25 +20,59 @@ insurance = gets.chomp
 
 #boolean checks
 if age.to_i == real_age 
-	lying = false
+	honest = true
 else
-	lying = true
+	honest = false
+	vamp_score +=2
 end
 
-puts lying
+puts honest
 
 if garlic == "Y" or garlic == "y"
-	allergic = true
-else
 	allergic = false
+else
+	allergic = true
+	vamp_score += 1
 end
 
 puts allergic
 
 if insurance == "Y" or insurance == "y"
-	immortal = true
-else
 	immortal = false
+else
+	immortal = true
+	vamp_score +=1
 end
 
 puts immortal
+
+if name == "Drake Cula" or name == "Tu Fang"
+  vamp_score += 10 
+end
+
+	puts "Your score is #{vamp_score}!"
+=begin wrong order
+	if vamp_score < 2 
+	  puts "probably not a vampire"
+	elsif vamp_score > 3 
+	  puts "results inconclusive"
+	elsif vamp_score > 4 
+	  puts "probably a vampire"
+	elsif vamp_score > 5 
+	  puts "almost certainly a vampire"
+	elsif vamp_score > 10
+	  puts "definitely a vampire"
+	end
+=end
+
+if vamp_score >= 10
+  puts "definitely a vampire"
+elsif vamp_score >= 4
+  puts "almost certainly a vampire"
+elsif vamp_score >= 3
+  puts "probably a vampire"
+elsif vamp_score >= 2 
+  puts "results inconclusive"
+else
+  puts "probably not a vampire"
+end
