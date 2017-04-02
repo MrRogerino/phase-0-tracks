7630a582ec8d1bf2f5631885c9b13e099b044893
Roger <weihsuan.li24@gmail.com>
2017-04-02 01:51:32 -0700

    Create loop to allow for multiple interviews

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,78 +1,90 @@
-vamp_score = 0
+puts "How many employees are we interviewing?"
+employees = gets.chomp 
 
-puts "What is your name?"
-name = gets.chomp
+employees_interviewed = 0
+until employees_interviewed == employees.to_i
+	puts "You've interviewed #{employees_interviewed} out of #{employees} employees"
 
-puts "How old are you?"
-age = gets.chomp
-
-
-puts "What year were you born?"
-birthyear = gets.chomp
-real_age = 2017 - birthyear.to_i
-puts real_age
-
-puts "Would you like some garlic bread? (Y or N)"
-garlic = gets.chomp
-
-puts "Do you need health insurance? (Y or N)"
-insurance = gets.chomp
-
-#boolean checks
-if age.to_i == real_age 
-	honest = true
-else
-	honest = false
-	vamp_score +=2
-end
-
-puts honest
-
-if garlic == "Y" or garlic == "y"
-	allergic = false
-else
-	allergic = true
-	vamp_score += 1
-end
-
-puts allergic
-
-if insurance == "Y" or insurance == "y"
-	immortal = false
-else
-	immortal = true
-	vamp_score +=1
-end
-
-puts immortal
-
-if name == "Drake Cula" or name == "Tu Fang"
-  vamp_score += 10 
-end
-
-	puts "Your score is #{vamp_score}!"
-=begin wrong order
-	if vamp_score < 2 
-	  puts "probably not a vampire"
-	elsif vamp_score > 3 
-	  puts "results inconclusive"
-	elsif vamp_score > 4 
-	  puts "probably a vampire"
-	elsif vamp_score > 5 
-	  puts "almost certainly a vampire"
-	elsif vamp_score > 10
-	  puts "definitely a vampire"
-	end
+	  vamp_score = 0
+	  
+	  puts "What is your name?"
+	  name = gets.chomp
+	  
+	  puts "How old are you?"
+	  age = gets.chomp
+	  
+	  
+	  puts "What year were you born?"
+	  birthyear = gets.chomp
+	  real_age = 2017 - birthyear.to_i
+	  puts "#{name}'s real age is #{real_age}."
+	  
+	  puts "Would you like some garlic bread? (Y or N)"
+	  garlic = gets.chomp
+	  
+	  puts "Do you need health insurance? (Y or N)"
+	  insurance = gets.chomp
+	  
+	  #boolean checks
+	  if age.to_i == real_age 
+	  	honest = true
+	  else
+	  	honest = false
+	  	vamp_score +=2
+	  end
+	  
+	  puts honest
+	  
+	  if garlic == "Y" or garlic == "y"
+	  	allergic = false
+	  else
+	  	allergic = true
+	  	vamp_score += 1
+	  end
+	  
+	  puts allergic
+	  
+	  if insurance == "Y" or insurance == "y"
+	  	immortal = false
+	  else
+	  	immortal = true
+	  	vamp_score +=1
+	  end
+	  
+	  puts immortal
+	  
+	  if name == "Drake Cula" or name == "Tu Fang"
+	    vamp_score += 10 
+	  end
+	  
+	  puts "#{name}'s score is #{vamp_score}!"
+	  	
+=begin 	WRONG ORDER
+	  	if vamp_score < 2 
+	  	  puts "probably not a vampire"
+	  	elsif vamp_score > 3 
+	  	  puts "results inconclusive"
+	  	elsif vamp_score > 4 
+	  	  puts "probably a vampire"
+	  	elsif vamp_score > 5 
+	  	  puts "almost certainly a vampire"
+	  	elsif vamp_score > 10
+	  	  puts "definitely a vampire"
+	  	end
 =end
+	  
+	  if vamp_score >= 10
+	    puts "#{name} is definitely a vampire"
+	  elsif vamp_score >= 4
+	    puts "#{name} is almost certainly a vampire"
+	  elsif vamp_score >= 3
+	    puts "#{name} is probably a vampire"
+	  elsif vamp_score >= 2 
+	    puts "results inconclusive"
+	  else
+	    puts "#{name} is probably not a vampire"
+	  end
+	  
+	  employees_interviewed +=1 
+	end
 
-if vamp_score >= 10
-  puts "definitely a vampire"
-elsif vamp_score >= 4
-  puts "almost certainly a vampire"
-elsif vamp_score >= 3
-  puts "probably a vampire"
-elsif vamp_score >= 2 
-  puts "results inconclusive"
-else
-  puts "probably not a vampire"
-end
