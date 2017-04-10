5b6627872db685b8922803293e751ffec205943b
MrRogerino <lakersli@gmail.com>
2017-04-09 21:23:32 -0700

    test delete_if and keep_if methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -18,7 +18,30 @@ teams = {
 	Heat: "Wade",
 }
 
+
 teams.each do |team|, |player|
 	puts "#{player} played for the #{team}"
 end
 
+#release 2
+
+array = [10,20,30,40,50,60,70]
+
+hash = {
+	a:1,
+	b:2,
+	c:3,
+	d:4,
+	e:5,
+	f:6,
+	g:7,
+}
+#delete if certain condition is met
+array.delete_if { |number| number % 20 == 0 }
+
+hash.delete_if { |letter, number| letter == "a" or letter == "c"}
+
+#keep if certain condition is met
+array.keep_if { |number| number < 35}
+
+hash.keep_if { |letter, number| number % 2 == 1}
