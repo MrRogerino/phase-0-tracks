0590cfc2329b90a0f56ea4731d456f9ee957fb2c
MrRogerino <lakersli@gmail.com>
2017-04-10 02:29:23 -0700

    Create encrypter function

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,18 @@
 #swaps first and last name
 
 def name_swap(str)
-	new_name = str.split(' ').reverse
-	return new_name
+  new_name = str.split(' ').reverse
+  return new_name
 end
 
 #consonant and vowel scrambler
 def next_consonant(str)
   letter = str.downcase
-	consonants = "bcdfghjklmnpqrstvwxyz"
-	counter = 0
-	until letter == consonants[counter]
-	  counter +=1 
-	end
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  counter = 0
+  until letter == consonants[counter]
+    counter +=1
+  end
   letter = consonants[counter + 1]
   if letter == nil #edge case
     return "b"
@@ -24,9 +24,9 @@ end
 def next_vowel(str)
   letter = str.downcase
   vowels = "aeiou"
-  counter = 0 
+  counter = 0
   until letter == vowels[counter]
-    counter +=1 
+    counter +=1
   end
   letter = vowels[counter + 1]
   if letter == nil #edge case
@@ -34,4 +34,37 @@ def next_vowel(str)
   else
     return letter
   end
+end
+
+def alias(str)
+  a = str.downcase
+  codename = ""
+  counter = 0
+  vowels = "aeiou"
+  until counter == a.length
+  	if vowels.include?(a[counter]) == true
+  		codename += next_vowel(a[counter])
+  		counter +=1 
+  	else
+  		new_word += next_consonant(a[counter])
+  		counter +=1
+  	end
+  end
+end
+
+def something(str)
+  a = str.downcase
+  counter = 0
+  new_word = ""
+  vowels = "aeiou"
+  until counter == a.length
+    if vowels.include?(a[counter]) == false
+      new_word += next_consonant(a[counter])
+      counter +=1 
+    else 
+      new_word += next_vowel(a[counter])
+      counter +=1
+    end
+  end
+  return new_word
 end
