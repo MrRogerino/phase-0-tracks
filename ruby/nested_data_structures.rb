01da3e3d16a406737386f5660fe34de5fabfa371
MrRogerino <lakersli@gmail.com>
2017-04-10 00:20:12 -0700

    Create nested structure about nba teams

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,52 @@
+nba = {
+  western_conference: {
+    divisions: {
+      pacific: [
+        "Lakers",
+        "Clippers",
+        "Warriors",
+        "Kings",
+        "Suns"
+        ],
+      northwest: [
+        "Jazz",
+        "Thunder",
+        "Trailblazers",
+        "Nuggets",
+        "Timberwolves"
+        ],
+      southwest: [
+        "Spurs",
+        "Rockets",
+        "Grizzlies",
+        "Pelicans",
+        "Mavericks"
+        ]
+    },
+  },
+  eastern_conference: {
+    divisions: {
+      atlantic: [
+        "Celtics",
+        "Raptors",
+        "Knicks",
+        "76ers",
+        "Nets"
+        ],
+      central: [
+        "Cavaliers",
+        "Bucks",
+        "Pacers",
+        "Bulls",
+        "Pistons"
+        ],
+      southeast: [
+        "Wizards",
+        "Hawks",
+        "Heat",
+        "Hornets",
+        "Magic"
+        ]
+    },
+  },
+}
