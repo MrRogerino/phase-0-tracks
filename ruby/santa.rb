62139fb732995ba00bfd4727e6559599b73ee35f
Roger Li <lakersli@gmail.com>
2017-04-16 23:57:12 -0600

    Define santa class and initial santa methods

---
+++ ruby/santa.rb
@@ -0,0 +1,13 @@
+class Santa
+  def initialize()
+    print "\n Initializing Santa instance... "
+  end
+
+  def speak()
+    print "\n Ho, ho ho! Haaaaaaaaapy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie)
+    print "\n That was a good #{cookie}!"
+  end
+end
