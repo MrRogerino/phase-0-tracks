e715ce970ffe5edbd17f60fb26877cd22b183280
Roger Li <lakersli@gmail.com>
2017-04-16 23:06:26 -0600

    Define class methods for puppy

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  	print "\n Woof!" * int
+  end
+
+  def roll_over()
+  	print "\n *rolls over*"
+  end
+
+  def dog_years(int)
+  	return int * 7
+  end
+
+  def run(distance)
+  	print "\n I ran for #{distance} meters!"
+  end
+
 end
 
+
+#driver code
+charles = Puppy.new
+
+charles.fetch("ball")
+charles.speak(8)
+charles.roll_over()
+charles.dog_years(15)
+charles.run(23)
