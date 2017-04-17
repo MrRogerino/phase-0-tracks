9251b89fc4d2ee0587c5c6d29c931500a579e350
Roger Li <lakersli@gmail.com>
2017-04-16 23:30:33 -0600

    Create baller class and define two baller methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -27,7 +27,6 @@ class Puppy
 
 end
 
-
 #driver code
 charles = Puppy.new
 
@@ -36,3 +35,28 @@ charles.speak(8)
 charles.roll_over()
 charles.dog_years(15)
 charles.run(23)
+
+class Baller
+
+  def initialize()
+    print "\n We ballin' now"
+  end
+  
+  def practice(shots)
+    print "\n You made #{shots/2} out of #{shots} shots in practice today"
+  end
+  
+  def mvp()
+    print "\n Unless your name is James Harden, you are not the MVP"
+  end
+    
+end
+
+#driver 
+kobe = Baller.new 
+kobe.practice(500)
+kobe.mvp()
+
+
+
+
