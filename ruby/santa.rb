7cdb55f865996325aa46d7cc833fb9b16b6db7ed
Roger Li <lakersli@gmail.com>
2017-04-17 01:51:39 -0600

    Define attribute changing methods and two getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,4 +15,47 @@ class Santa
   def eat_milk_and_cookies(cookie)
     print "\n That was a good #{cookie}!"
   end
+
+  def celebrate_birthday
+    @age +=1
+  end
+
+  def get_mad_at(reindeer)
+    if @reindeer_ranking.include?(reindeer.capitalize)
+      @reindeer_ranking -= [reindeer]
+      @reindeer_ranking += [reindeer]
+    else
+      print "\n #{reindeer} is not a real reindeer!"
+    end
+  end
+
+  def gender(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    print "\n #{@age}"
+  end
+
+  def ethnicity
+    print "\n #{@ethnicity}"
+  end
+
 end
+
+#driver
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+#- - - - - -
+kris = Santa.new("male", "black")
+kris.age
+kris.ethnicity
+kris.celebrate_birthday
+kris.age
+kris.gender("female")
