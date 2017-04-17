45a9de5876da5f19c5ea441c38295d26a5e623cd
Roger Li <lakersli@gmail.com>
2017-04-16 22:48:50 -0600

    Design a puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin 
+
+Puppy Class
+
+Characteristics 
+---------------
+Name: Varies
+Age: Varies
+Species: Varies
+Loyal: Always
+Cute: Definitely
+
+Behavior
+--------
+Fetch
+Bark
+Poop
+Sleep
+
+=end
