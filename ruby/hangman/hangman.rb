8d0112c29e980ceea51485e6264e8e2bf4adde48
MrRogerino <lakersli@gmail.com>
2017-04-19 17:49:47 -0700

    Add guess functionality

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,16 +1,35 @@
 class Hangman
-  attr_accessor :answer_array
-	def initialize(word)
-		@answer_array = word.downcase.split('')
-		@guess_array = []
-		word.length.times do 
-			@guess_array << "_"
-			end
-		@guesses_allowed = 1
-		@game_over = false 
-	end
+  attr_reader :answer_array,:guesses_allowed
+  attr_accessor :guess_array
+
+  def initialize(word)
+    @answer_array = word.downcase.split('')
+    @guess_array = []
+    word.length.times do
+      @guess_array << "_"
+    end
+    @guesses_allowed = 1
+    @game_over = false
+  end
+
+  def check(letter)
+    if @answer_array.include?(letter)
+      match = @answer_array.each_index.select{|i| @answer_array[i] == letter}
+      match.each {|a| @guess_array[a] = letter}
+    end
+  end
 end
 
+
+#driver
 puts "Player 1, what word would like you Player 2 to guess?"
-hangman = gets.chomp 
+hangman = gets.chomp
 game = Hangman.new(hangman)
+p game.answer_array
+p game.guess_array
+game.guesses_allowed
+puts "Player 2, guess a letter"
+guess = gets.chomp
+game.check(guess)
+p game.guess_array
+game.guesses_allowed
