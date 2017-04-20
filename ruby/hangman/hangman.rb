a2ff4264d88107c997cde343f71c52c764aaad0f
MrRogerino <lakersli@gmail.com>
2017-04-19 20:06:10 -0700

    Add game loop for basic guessing

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,5 +1,5 @@
 class Hangman
-  attr_reader :answer_array,:guesses_allowed
+  attr_reader :answer_array,:guesses_allowed, :game_over
   attr_accessor :guess_array
 
   def initialize(word)
@@ -17,19 +17,24 @@ class Hangman
       match = @answer_array.each_index.select{|i| @answer_array[i] == letter}
       match.each {|a| @guess_array[a] = letter}
     end
+    if @answer_array == @guess_array
+      @game_over = true
+    end
   end
 end
 
 
 #driver
 puts "Player 1, what word would like you Player 2 to guess?"
-hangman = gets.chomp
-game = Hangman.new(hangman)
-p game.answer_array
-p game.guess_array
-game.guesses_allowed
-puts "Player 2, guess a letter"
-guess = gets.chomp
-game.check(guess)
-p game.guess_array
-game.guesses_allowed
+word = gets.chomp
+hangman = Hangman.new(word)
+p hangman.answer_array
+p hangman.guess_array
+until hangman.game_over
+  puts "Player 2, guess a letter"
+  guess = gets.chomp
+  hangman.check(guess)
+end
+if hangman.game_over
+  puts "GG!"
+end
