df4ddb73bedf4946f48327aba5710d709fc5360e
MrRogerino <lakersli@gmail.com>
2017-04-20 11:03:33 -0700

    Add guess counter and endgame states

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -8,7 +8,7 @@ class Hangman
     word.length.times do
       @guess_array << "_"
     end
-    @guesses_allowed = 1
+    @guesses_allowed = word.length + 3
     @game_over = false
   end
 
@@ -16,13 +16,14 @@ class Hangman
     if @answer_array.include?(letter)
       match = @answer_array.each_index.select{|i| @answer_array[i] == letter}
       match.each {|a| @guess_array[a] = letter}
+      p @guess_array
     end
     if @answer_array == @guess_array
       @game_over = true
     end
+    @guesses_allowed -= 1 
   end
 end
-
 
 #driver
 puts "Player 1, what word would like you Player 2 to guess?"
@@ -32,9 +33,14 @@ p hangman.answer_array
 p hangman.guess_array
 until hangman.game_over
   puts "Player 2, guess a letter"
+  puts "You have #{hangman.guesses_allowed} guesses left!"
   guess = gets.chomp
   hangman.check(guess)
+  if hangman.guesses_allowed == 0
+    puts "You have no more guesses! Try harder next time!"
+    break
+  end
 end
 if hangman.game_over
-  puts "GG!"
+  puts "GG! You won hangman!"
 end
