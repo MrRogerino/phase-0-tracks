954c9e356827e2fc8fd6cfcf5a10a8d2e8868cc0
MrRogerino <lakersli@gmail.com>
2017-04-21 16:22:56 -0700

    Create function to pair each horse with a color

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,25 @@
+//Release 0: Work with Arrays
 var color_array = ["blue", "turqoise", "cyan", "cerulean"]
 var horsie = ["Rick", "Morty","Summer","Beth"]
 
 color_array.push("navy")
 horsie.push("Jerry")
+//Release 1: Build an Object
+function stable(horsie, color_array) {
+  if (horsie.length > color_array.length) {
+    console.log("Some horsies won't have a color!")
+  }
+  else if (color_array.length > horsie.length) {
+    console.log("Some colors won't have a horsie!")
+  }
+  else {
+  pair = {};
+  for (var i = 0; i < color_array.length; i++) {
+    pair[horsie[i]] = color_array[i];
+  }
+  return pair;
+  }
+}
+
+stable(horsie, color_array)
+stable(horsie, color_array)
