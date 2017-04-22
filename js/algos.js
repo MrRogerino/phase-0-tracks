54b2d65fc71a7f92eff11562b83bf569d3350c61
MrRogerino <lakersli@gmail.com>
2017-04-21 18:51:15 -0700

    Psuedocode for longest string

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+/*Release 0 Psuedocode
+Define a function that takes in an array.
+  For each element in the array, store the length of the element in a new array
+    From the new array, take the index of the highest value(s)
+  Return the element that corresponds to those index
+
