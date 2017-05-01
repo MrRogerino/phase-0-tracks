7dada0e3a84bacd5988bd559b6f4233ee148ccae
MrRogerino <lakersli@gmail.com>
2017-05-01 03:04:40 -0700

    Define create-problem method

--- databases/mental-math/mental_math.rb
+++ databases/mental-math/mental_math.rb
@@ -1,7 +1,7 @@
 require 'sqlite3'
 
-def start_test
-  db =  SQLITE3::Database.new("problems.db")
+
+  db = SQLite3::Database.new("problems.db")
   db.results_as_hash = true
   create_table_cmd = <<-SQL 
     CREATE TABLE IF NOT EXISTS problems (
@@ -9,11 +9,38 @@ def start_test
       first_num INT,
       second_num INT,
       operand CHAR(1),
-      correct_answer INT,
-      user_answer INT
+      right_answer INT,
+      user_answer INT,
+      correct BOOLEAN
     )
   SQL
+
   db.execute(create_table_cmd)
   p "Welcome to Mental Math 101!"
-end
 
+
+def create_problem(db, operand, difficulty)
+  max = difficulty * 10 
+  first_num = rand(1..max)
+  second_num = rand(1..max)
+  if operand == "+"
+    puts "#{first_num} + #{second_num} equals?"
+    right_answer = first_num + second_num
+  end
+  if operand == "-"
+    puts "#{first_num} - #{second_num} equals?"
+    right_answer = first_num - second_num
+  end
+  user_answer = gets.chomp.to_i
+  if user_answer == right_answer
+    puts "Correct!"
+    correct = true
+  else
+    puts "Incorrect! The answer was #{right_answer}!"
+    correct = false
+  end
+  db.execute("INSERT INTO problems (first_num, second_num, operand, right_answer, user_answer) VALUES [?,?,?,?,?,?]", [first_num, second_num, operand, right_answer, user_answer, correct])
+end 
+
+#driver
+create_problem(db, "+", 2)
